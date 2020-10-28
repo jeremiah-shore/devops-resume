54abbd533ff55b780197b3db26d8bc43313605a8
shore.jeremiah@gmail.com <shore.jeremiah@gmail.com>
2020-10-28 16:43:07 -0600

    removed markup related to intended styling

--- src/components/tech-skills/tech-skills.js
+++ src/components/tech-skills/tech-skills.js
@@ -11,43 +11,38 @@ export function TechSkills() {
     >
 
         <SkillList>
-          <div className="col-left">
-            <SkillLi
-              title={"code"}
-              skills={['Java', 'TypeScript', 'JavaScript', 'Node', 'SQL', 'NoSQL', 'C#']}
-            />
-            <SkillLi
-              title={"AWS"}
-              skills={['AWS-CLI',  'Lambda', 'Cloudfront', 'CodeBuild', 'CodePipeline', 'S3', 'DynamoDB', 'ElasticSearch', 'Route 53', 'EC2']}
-            />
-            <SkillLi
-              title={"libraries"}
-              skills={['Lombok', 'Jackson', 'AsyncHttp', 'Puppeteer', 'Gson']}
-            />
-            <SkillLi
-              title={"frameworks"}
-              skills={['SparkJava', 'JUnit', 'Mockito', 'Spring', 'Jest', 'jQuery', 'JPA / Hibernate', 'Bootstrap', 'MonoBehavior', '.NET']}
-            />
-          </div>
-          
-          <div className="col-right">
-            <SkillLi
-              title={"web"}
-              skills={['REST', 'HTML', 'CSS', 'AJAX', 'Amazon Web Services', 'Google Search Console', 'Google Analytics', 'WordPress', 'WIX']}
-            />
-            <SkillLi
-              title={"dev tools"}
-              skills={['IntelliJ', 'Git', 'Maven', 'Postman', 'VS Code', 'Unity', 'Docker', 'Gradle', ]}
-            />
-            <SkillLi
-              title={"workflow"}
-              skills={['GitHub', 'Jira', 'GSuite', 'MS Office', 'Trello', 'Slack', 'Zapier']}
-            />
-            <SkillLi
-              title={"network"}
-              skills={['TCP/IP', 'LAN', 'DNS', 'FTP', 'firewall/ACL', 'WAN']}
-            />
-          </div>
+          <SkillLi
+            title={"code"}
+            skills={['Java', 'TypeScript', 'JavaScript', 'Node', 'SQL', 'NoSQL', 'C#']}
+          />
+          <SkillLi
+            title={"AWS"}
+            skills={['AWS-CLI',  'Lambda', 'Cloudfront', 'CodeBuild', 'CodePipeline', 'S3', 'DynamoDB', 'ElasticSearch', 'Route 53', 'EC2']}
+          />
+          <SkillLi
+            title={"libraries"}
+            skills={['Lombok', 'Jackson', 'AsyncHttp', 'Puppeteer', 'Gson']}
+          />
+          <SkillLi
+            title={"frameworks"}
+            skills={['SparkJava', 'JUnit', 'Mockito', 'Spring', 'Jest', 'jQuery', 'JPA / Hibernate', 'Bootstrap', 'MonoBehavior', '.NET']}
+          />
+          <SkillLi
+            title={"web"}
+            skills={['REST', 'HTML', 'CSS', 'AJAX', 'Amazon Web Services', 'Google Search Console', 'Google Analytics', 'WordPress', 'WIX']}
+          />
+          <SkillLi
+            title={"dev tools"}
+            skills={['IntelliJ', 'Git', 'Maven', 'Postman', 'VS Code', 'Unity', 'Docker', 'Gradle', ]}
+          />
+          <SkillLi
+            title={"workflow"}
+            skills={['GitHub', 'Jira', 'GSuite', 'MS Office', 'Trello', 'Slack', 'Zapier']}
+          />
+          <SkillLi
+            title={"network"}
+            skills={['TCP/IP', 'LAN', 'DNS', 'FTP', 'firewall/ACL', 'WAN']}
+          />
         </SkillList>
       
       
