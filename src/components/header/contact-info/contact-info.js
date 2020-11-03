1dcb46f13a0bf8a1bdb705d1534799595d0f94f7
shore.jeremiah@gmail.com <shore.jeremiah@gmail.com>
2020-11-03 10:52:05 -0700

    styled contact info for mobile

--- src/components/header/contact-info/contact-info.js
+++ src/components/header/contact-info/contact-info.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './contact-info.scss';
+import {Email, Map, Phone} from "@material-ui/icons";
 
 export function ContactInfo() {
   return (
     <div className="contact">
       <ul className="contact__list">
         <li className="contact__list--address">
-          Denver, CO 80247
+          <Map fontSize={"large"} />
+          <a
+            href="https://goo.gl/maps/u4n8teefqfNWgsjq5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Denver, CO 80247
+          </a>
         </li>
         <li className="contact__list--email">
+          <Email fontSize={"large"} />
           <a href="mailto:contact@jeremiahshore.net">
             contact@jeremiahshore.net
           </a>
         </li>
         <li className="contact__list--phone">
+          <Phone fontSize={"large"} />
           <a href="tel:7205775465">
             (720) 577-5465
           </a>
