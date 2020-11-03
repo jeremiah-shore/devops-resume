ffca73f40205c83642c5455002f624a322a7d2ad
shore.jeremiah@gmail.com <shore.jeremiah@gmail.com>
2020-11-03 11:26:31 -0700

    styled footer for mobile

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 export function Footer() {
   return (
-    <footer>
+    <footer
+      style={{
+        fontStyle: "italic",
+        fontWeight: 300,
+      }}
+    >
+      <div
+        style={{
+          height: "2px",
+          backgroundColor: "rgba(0, 0, 0, 0.1)",
+          margin: "2rem 0",
+          borderRadius: "1px"
+        }}
+      />
       References, work history—including 9+ years of IT industry experience—and certifications made available upon request.
     </footer>
   )
