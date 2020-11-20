220b30725004e2950d9d0d81d2064ed0c7f9143c
shore.jeremiah@gmail.com <shore.jeremiah@gmail.com>
2020-11-20 11:52:55 -0700

    rendered keywords as chips

--- src/components/admin-panel/admin-panel.js
+++ src/components/admin-panel/admin-panel.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import TextField from "@material-ui/core/TextField";
 import {AddCircle} from "@material-ui/icons";
+import Chip from "@material-ui/core/Chip";
 
 export function AdminPanel(props) {
   const [encouragedKeywords, setEncouragedKeywords] = useState(["Java", "React", "AWS"]);
@@ -19,29 +20,31 @@ export function AdminPanel(props) {
         }}
       >
         <div style={{position: 'relative', display: 'inline-block'}}>
-          <AddCircle style={{position: 'absolute', right: 0, top: 15, width: 20, height: 20, color: "rgba(0,0,0,0.5)"}}/>
+          <AddCircle style={{position: 'absolute', right: 0, top: 15, width: 20, height: 20, color: "rgba(0,0,0,0.25)"}}/>
           <TextField
             label={"add encouraged keyword"}
             style={{
               width: "300px",
             }}
             onKeyDown={e => {
-              if (e.key === 'Enter') {
+              if (e.key === 'Enter' && e.target.value) {
                 addEkw(e.target.value);
+                e.target.value = '';
               }
             }}
           />
         </div>
         <div style={{position: 'relative', display: 'inline-block'}}>
-          <AddCircle style={{position: 'absolute', right: 0, top: 15, width: 20, height: 20, color: "rgba(0,0,0,0.5)"}}/>
+          <AddCircle style={{position: 'absolute', right: 0, top: 15, width: 20, height: 20, color: "rgba(0,0,0,0.25)"}}/>
           <TextField
             label={"add discouraged keyword"}
             style={{
               width: "300px",
             }}
             onKeyDown={e => {
-              if (e.key === 'Enter') {
+              if (e.key === 'Enter' && e.target.value) {
                 addDkw(e.target.value);
+                e.target.value = '';
               }
             }}
           />
@@ -52,24 +55,36 @@ export function AdminPanel(props) {
         style={{
           padding: "10px",
           display: "flex",
-          justifyContent: "space-around",
+          justifyContent: "space-evenly",
           textAlign: "center",
         }}
       >
-        <div className="encouraged">
+        <div className="encouraged" style={{ maxWidth: "50%" }}>
           <p style={{ fontWeight: "600", padding: "1rem" }}>
             encouraged keywords
           </p>
           <p style={{ fontWeight: "300" }}>
-            {encouragedKeywords.join(', ')}
+            {encouragedKeywords.map(kw =>
+              <Chip
+                label={kw}
+                onDelete={() => {}}
+                style={{ fontSize: "1.5rem", margin: "0 5px 5px"}}
+              />
+            )}
           </p>
         </div>
-        <div className="discouraged">
+        <div className="discouraged" style={{ maxWidth: "50%" }}>
           <p style={{ fontWeight: "600", padding: "1rem" }}>
             discouraged keywords
           </p>
           <p style={{ fontWeight: "300" }}>
-            {discouragedKeywords.join(', ')}
+            {discouragedKeywords.map(kw =>
+              <Chip
+                label={kw}
+                onDelete={() => {}}
+                style={{ fontSize: "1.5rem", margin: "0 5px 10px"}}
+              />
+            )}
           </p>
         </div>
         
