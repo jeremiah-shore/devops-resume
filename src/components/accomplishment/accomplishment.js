094c302d27bb413e2d032e57ccfe7fc0e9746f94
shore.jeremiah@gmail.com <shore.jeremiah@gmail.com>
2020-11-21 20:03:31 -0700

    added conditional display logic to accomplishment keywords

--- src/components/accomplishment/accomplishment.js
+++ src/components/accomplishment/accomplishment.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export function Accomplishment(props) {
   return (
-    <li className={"accomplishment"}>{props.description} ({props.keywords?.join(', ')})</li>
+    <li className={"accomplishment"}>
+      {props.description}
+      
+      { (props.keywords && props.keywords.length > 0) &&
+        ` (${props.keywords?.join(', ')})`
+      }
+    </li>
   )
 }
